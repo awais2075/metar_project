d70567bf26a858e30bbad9d0d60af2155c63d15a
awais2075 <awais2075@gmail.com>
2022-02-22 00:00:50 +0500

    initial-commit

---
+++ src/main/java/com/metar/MetarApplication.java
@@ -0,0 +1,16 @@
+package com.metar;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class MetarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/metar/MetarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
