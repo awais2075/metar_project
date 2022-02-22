25d69be42d09a7f04a8f0962b53db2b58e24cbed
awais2075 <awais2075@gmail.com>
2022-02-22 23:58:27 +0500

    added jqpl-query-method findByIcaoCode

--- src/main/java/com/metar/repository/SubscriptionRepository.java
+++ src/main/java/com/metar/repository/SubscriptionRepository.java
@@ -2,8 +2,17 @@ package com.metar.repository;
 
 import com.metar.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    @Query("select s from Subscription s where s.icaoCode = :icaoCode")
+    Optional<Subscription> findByIcaoCode(@Param("icaoCode") String icaoCode);
+
+
 }
