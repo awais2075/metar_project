1455740c7b84a7c63cb844cd361469c459f2ca6c
awais2075 <awais2075@gmail.com>
2022-02-23 00:05:24 +0500

    updated service-methods to save-metar [throw SubscriptionNotFoundException] and getMetar [throw SubscriptionNotFoundException]

--- src/main/java/com/metar/service/MetarService.java
+++ src/main/java/com/metar/service/MetarService.java
@@ -1,11 +1,14 @@
 package com.metar.service;
 
 import com.metar.entity.Metar;
+import com.metar.exception.MetarNotFoundException;
+import com.metar.exception.SubscriptionNotFoundException;
 import com.metar.repository.MetarRepository;
+import com.metar.repository.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Objects;
 
 @Service
 public class MetarService {
@@ -13,11 +16,29 @@ public class MetarService {
     @Autowired
     private MetarRepository metarRepository;
 
-    public List<Metar> getMetars(String icaoCode) {
-        return metarRepository.findByIcaoCode(icaoCode);
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    public Metar getMetars(String icaoCode) throws SubscriptionNotFoundException, MetarNotFoundException {
+        var subscription = subscriptionRepository.findByIcaoCode(icaoCode).orElse(null);
+        if(Objects.isNull(subscription)) {
+            throw new SubscriptionNotFoundException(icaoCode+" subscription not found in list");
+        }
+
+        var metar = metarRepository.findFirstBySubscription_IcaoCodeEqualsOrderByIdDesc(icaoCode).orElse(null);
+        if(Objects.isNull(metar)) {
+            throw new MetarNotFoundException("Metar Data not found against icaoCode : "+icaoCode);
+        }
+
+        return metar;
     }
 
-    public Metar addMetar(String icaoCode, Metar metar) {
+    public Metar addMetar(String icaoCode, Metar metar) throws SubscriptionNotFoundException {
+        var subscription = subscriptionRepository.findByIcaoCode(icaoCode).orElse(null);
+        if(Objects.isNull(subscription)) {
+            throw new SubscriptionNotFoundException(icaoCode+" subscription not found in list");
+        }
+        metar.setSubscription(subscription);
         return metarRepository.save(metar);
     }
 }
