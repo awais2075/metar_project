9e939761e6f945761e455b7b44443e3295482097
awais2075 <awais2075@gmail.com>
2022-02-23 00:02:15 +0500

    updated addSubscription service-method [throw SubscrptionFoundException - duplicate-check]

--- src/main/java/com/metar/service/SubscriptionService.java
+++ src/main/java/com/metar/service/SubscriptionService.java
@@ -1,11 +1,13 @@
 package com.metar.service;
 
 import com.metar.entity.Subscription;
+import com.metar.exception.SubscriptionFoundException;
 import com.metar.repository.SubscriptionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class SubscriptionService {
@@ -17,7 +19,11 @@ public class SubscriptionService {
         return subscriptionRepository.findAll();
     }
 
-    public Subscription addSubscription(Subscription subscription) {
+    public Subscription addSubscription(Subscription subscription) throws SubscriptionFoundException {
+        var sub = subscriptionRepository.findByIcaoCode(subscription.getIcaoCode()).orElse(null);
+        if(Objects.nonNull(sub)) {
+            throw new SubscriptionFoundException(subscription.getIcaoCode()+ " already exists in subscription list");
+        }
         return subscriptionRepository.save(subscription);
     }
 }
