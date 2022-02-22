ca32bd0e55bec507c7f8b65b7d3bd45fd87afca7
awais2075 <awais2075@gmail.com>
2022-02-22 23:59:50 +0500

    added annotations not-null, not-blank validation constraints on data and json-ignore on subscription

--- src/main/java/com/metar/entity/Metar.java
+++ src/main/java/com/metar/entity/Metar.java
@@ -1,11 +1,14 @@
 package com.metar.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "metar")
@@ -19,11 +22,14 @@ public class Metar {
     private Long id;
 
     @Column(name = "data", nullable = false)
+    @NotNull(message = "{data.notnull}")
+    @NotBlank(message = "{data.notblank}")
     private String data;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "subscription_id", nullable = false)
     @Hidden
+    @JsonIgnore
     private Subscription subscription;
 
 }
