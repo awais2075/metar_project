1d3e606df6026c5e72ef630f3a5ebc6f73ac8ffe
awais2075 <awais2075@gmail.com>
2022-02-22 23:57:06 +0500

    added jpql-query-methods --- findAllByIcaoCode and findFIrstBySubscription_IcaoCodeEqualsOrderByDesc

--- src/main/java/com/metar/repository/MetarRepository.java
+++ src/main/java/com/metar/repository/MetarRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MetarRepository extends JpaRepository<Metar, Long> {
     @Query("select m from Metar m where m.subscription.icaoCode = ?1")
-    List<Metar> findByIcaoCode(String icaoCode);
+    List<Metar> findAllByIcaoCode(String icaoCode);
+
+    Optional<Metar> findFirstBySubscription_IcaoCodeEqualsOrderByIdDesc(String icaoCode);
+
 
 }
