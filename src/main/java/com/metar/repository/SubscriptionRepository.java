ea9a7c7eebb079736ce0970b3d9833467aad0eec
awais2075 <awais2075@gmail.com>
2022-02-23 23:45:01 +0500

    added jpql-method deleteBySubscriptionId

--- src/main/java/com/metar/repository/SubscriptionRepository.java
+++ src/main/java/com/metar/repository/SubscriptionRepository.java
@@ -2,9 +2,11 @@ package com.metar.repository;
 
 import com.metar.entity.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,5 +16,8 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
     @Query("select s from Subscription s where s.icaoCode = :icaoCode")
     Optional<Subscription> findByIcaoCode(@Param("icaoCode") String icaoCode);
 
+    @Modifying
+    @Query("delete from Subscription s where s.id = :id")
+    Integer deleteBySubscriptionId(@Param("id") Long id);
 
 }
