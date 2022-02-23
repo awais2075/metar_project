08111b4a33c40afe1736a42db284eaaff462b75d
awais2075 <awais2075@gmail.com>
2022-02-24 01:55:41 +0500

    added ObjectNode Bean

--- src/main/java/com/metar/MetarApplication.java
+++ src/main/java/com/metar/MetarApplication.java
@@ -1,16 +1,21 @@
 package com.metar;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MetarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MetarApplication.class, args);
+	}
+
+	@Bean
+	public ObjectNode objectMapper() {
+		return new ObjectMapper().createObjectNode();
 	}
 
 }
