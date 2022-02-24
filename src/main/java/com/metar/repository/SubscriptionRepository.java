dc41bc03d7cb7398f26fbf9a855e70e1092261ba
awais2075 <awais2075@gmail.com>
2022-02-25 01:40:01 +0500

    added jpql-query-method to find allActiveSubscriptions

--- src/main/java/com/metar/repository/SubscriptionRepository.java
+++ src/main/java/com/metar/repository/SubscriptionRepository.java
@@ -1,12 +1,13 @@
 package com.metar.repository;
 
 import com.metar.entity.Subscription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -19,5 +20,8 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
     @Modifying
     @Query("delete from Subscription s where s.id = :id")
     Integer deleteBySubscriptionId(@Param("id") Long id);
+
+    @Query("select s from Subscription s where s.active = :active")
+    Page<Subscription> findAllActiveSubscriptions(@Param("active") Boolean active, Pageable pageable);
 
 }
