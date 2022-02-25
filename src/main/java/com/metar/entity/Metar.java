7136a44e1576e5825b4b407e1c6c62c75496fdc1
awais2075 <awais2075@gmail.com>
2022-02-26 01:03:36 +0500

    added annotations @JsonInclude(JsonInclude.Include.NON_NULL) to hide null values

--- src/main/java/com/metar/entity/Metar.java
+++ src/main/java/com/metar/entity/Metar.java
@@ -1,6 +1,7 @@
 package com.metar.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AccessLevel;
 import lombok.Data;
@@ -18,36 +19,44 @@ public class Metar {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long id;
 
     @Column(name = "data", nullable = false)
     @NotNull(message = "{data.notnull}")
     @NotBlank(message = "{data.notblank}")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String data;
 
     @Column(name = "timestamp")
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String timestamp;
 
     @Column(name = "wind")
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String wind;
 
     @Column(name = "visibility")
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String visibility;
 
     @Column(name = "temperature")
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String temperature;
 
     @Column(name = "dew")
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String dew;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "subscription_id", nullable = false)
     @Hidden
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonIgnore
     private Subscription subscription;
 
